fc7911049ce97ee8554d92a5bda8b450d0afd990
Anant-kumar1234 <33875854+Anant-kumar1234@users.noreply.github.com>
2018-08-12 12:44:49 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,70 @@
+<div class="header" id="home">
+		<div class="top_menu_w3layouts">
+<div class="container">
+			<div class="header_left">
+				<ul>
+					<li><i class="10.jpg" aria-hidden="true"></i> Address</li>
+					<li><i class="fa fa-phone" aria-hidden="true"></i> number</li>
+					<li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:info@example.com">info@example.com</a></li>
+				</ul>
+			</div>
+			<div class="header_right">
+				<ul class="forms_right">
+					<li><a href="appointment.php"> Make an Appointment</a> </li>
+					<li><a href="admin.php">Admin Login</a> </li>
+				</ul>
+
+			</div>
+			<div class="clearfix"> </div>
+			</div>
+		</div>
+
+		<div class="content white">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+						<a class="navbar-brand" href="index.php">
+							<h1><span class="10.jpg" aria-hidden="true"></span>Swastha Rajisthan </h1>
+						</a>
+					</div>
+					<!--/.navbar-header-->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<nav>
+							<ul class="nav navbar-nav">
+								<li><a href="index.php" class="active">Home</a></li>
+								<li><a href="about.php">About</a></li>
+								
+								<li><a href="departments.php">Departments</a></li>
+								<li><a href="gallery.php">Gallery</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Hospital <b class="caret"></b></a>
+									<ul class="dropdown-menu">
+										<li><a href="codes.php">Hospital1</a></li>
+										<li class="divider"></li>
+										<li><a href="icons.php">Hospital2</a></li>
+										<li class="divider"></li>
+										<li><a href="codes.php">Hospital3</a></li>
+										<li class="divider"></li>
+										<li><a href="icons.php">Hospital4</a></li>
+										<li class="divider"></li>
+										
+
+										
+									</ul>
+								</li>
+								<li><a href="mail.php">Mail Us</a></li>
+							</ul>
+						</nav>
+					</div>
+					<!--/.navbar-collapse-->
+					<!--/.navbar-->
+				</div>
+			</nav>
+		</div>
+	</div>
